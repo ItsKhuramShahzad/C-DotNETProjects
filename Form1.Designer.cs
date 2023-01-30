357ba3254f73dbf8fd7a3d0bb9843df53d13167c
KhuramShahzad <KhuramShahzad@ANJUM>
2023-01-30 11:14:01 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CustomDataaDridViewCell
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Azure;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.Location = new System.Drawing.Point(37, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowTemplate.Height = 70;
+            this.dataGridView1.Size = new System.Drawing.Size(682, 341);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Custom DataGridView Cell";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomDataaDridViewCell
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CustomCheckBoxColumn col = new CustomCheckBoxColumn();
+            col.Width = 130;
+            col.Label = "this is a test string";
+            col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            this.dataGridView1.Columns.Add(col);
+           
+            
+            CustomCheckBoxColumn col1 = new CustomCheckBoxColumn();
+            col.Width = 130;
+            col.Label = "this is a test string";
+            col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            this.dataGridView1.Columns.Add(col1);
+            CustomCheckBoxColumn col2 = new CustomCheckBoxColumn();
+            col.Width = 130;
+            col.Label = "this is a test string";
+            col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            this.dataGridView1.Columns.Add(col2);
+     
+            this.dataGridView1.RowCount = 1;
+
+            foreach (DataGridViewRow row in this.dataGridView1.Rows)
+            {
+                ((CustomCheckBoxCell)row.Cells[0]).Label = " test string";
+                //((CustomCheckBoxCell)row.Cells[0]).Icon = Image.FromFile(@"D:\All EPOS's 2020\MABH-Comon-EPOS\Quantum-POS 08-Jan-2023\EPOS\Resources\ma icons.ico");
+               
+                ((CustomCheckBoxCell)row.Cells[1]).Label = "test string";
+               // ((CustomCheckBoxCell)row.Cells[1]).Icon = Image.FromFile(@"D:\All EPOS's 2020\MABH-Comon-EPOS\Quantum-POS 08-Jan-2023\EPOS\Resources\ma icons.ico");
+                ((CustomCheckBoxCell)row.Cells[2]).Label = "test string";
+                //((CustomCheckBoxCell)row.Cells[2]).Icon = Image.FromFile(@"D:\All EPOS's 2020\MABH-Comon-EPOS\Quantum-POS 08-Jan-2023\EPOS\Resources\ma icons.ico");
+
+                dataGridView1.Refresh();
+            }
+        }
+        public class CustomCheckBoxColumn : DataGridViewCheckBoxColumn
+        {
+            private string label;
+
+            public string Label
+            {
+                get { return label; }
+                set { label = value; }
+            }
+
+            public override DataGridViewCell CellTemplate
+            {
+                get { return new CustomCheckBoxCell(); }
+            }
+        }
+
+        public class CustomCheckBoxCell : DataGridViewCheckBoxCell
+        {
+            // checkbox string   
+            private string label;
+
+            public string Label
+            {
+                get { return label; }
+                set { label = value; }
+            }
+
+            // checkbox icon  
+            private Image icon;
+
+            public Image Icon
+            {
+                get { return icon; }
+                set { icon = value; }
+            }
+
+            // override Paint  
+            protected override void Paint(Graphics graphics, Rectangle clipBounds, Rectangle cellBounds, int rowIndex, DataGridViewElementStates elementState, object value, object formattedValue, string errorText, DataGridViewCellStyle cellStyle, DataGridViewAdvancedBorderStyle advancedBorderStyle, DataGridViewPaintParts paintParts)
+            {
+
+                // the base Paint  
+                base.Paint(graphics, clipBounds, cellBounds, rowIndex, elementState, value, formattedValue, errorText, cellStyle, advancedBorderStyle, paintParts);
+
+                // get the check box bounds  
+                Rectangle contentBounds = this.GetContentBounds(rowIndex);
+
+                // the string location  
+                Point stringLocation = new Point();
+                stringLocation.Y = cellBounds.Y + 10;
+                stringLocation.X = cellBounds.X;
+
+
+                // paint the string.  
+                if (this.Label == null)
+                {
+                    CustomCheckBoxColumn col = (CustomCheckBoxColumn)this.OwningColumn;
+                    this.label = col.Label;
+                }
+
+                graphics.DrawString(
+                this.Label, Control.DefaultFont, System.Drawing.Brushes.Black, stringLocation);
+
+
+                // the icon rectangel  
+                Rectangle rectangle = new Rectangle(cellBounds.X + 15, cellBounds.Y + 2, 15, 15);
+                // paint icon  
+               //graphics.DrawImage(icon, rectangle);
+            }
+        }
+    }
+}
